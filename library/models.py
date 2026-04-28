fa05aaa34c449f04f466197e075969c2c21f10a5
Aleksey <crafter2312200@gmail.com>
2026-04-28 11:27:32 +0300

    making models in library app

--- library/models.py
+++ library/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    bio = models.TextField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    summary = models.TextField()
+    published_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title}"
